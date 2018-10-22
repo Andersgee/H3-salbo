434aefad9b6e738cebd21fb0e7f96d23dfe65caf
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2018-10-22 12:57:22 +0200

    platform-dependent halite binary

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "~/dev/julia-1.0.1/bin/julia MyBot.jl" "~/dev/julia-1.0.1/bin/julia MyBot.jl"
+if [ "$(uname)" == "Darwin" ]; then
+  JULIA=julia
+  HALITE=./halite-osx
+else
+  JULIA=~/dev/julia-1.0.1/bin/julia
+  HALITE=./halite-linux
+fi
+
+mkdir -p replays
+
+$HALITE --replay-directory replays/ -vvv --width 32 --height 32 "$JULIA MyBot.jl" "$JULIA MyBot.jl"
