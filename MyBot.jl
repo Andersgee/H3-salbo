fcf5ab508c1891795a6a2b0960c2fae5d369f89b
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2018-10-23 13:40:47 +0200

    mine if cannot move

--- MyBot.jl
+++ MyBot.jl
@@ -1,6 +1,8 @@
 include("hlt/halite.jl")
 include("salboai/salboai.jl")
 
+warn(s...) = println(Base.stderr, s...)
+
 using Main.Salboai
 
 miningthreshold = 9 #free to move after that. dont bother mining more?
@@ -14,8 +16,9 @@ ms, dir, cost_here2there = mapscore(g.halite, H.Ship(0, 0, CartesianIndex(1,1), 
 H.ready("salboai")
 
 while true
-	cmds=String[]
+	cmds = String[]
 	turn = H.update_frame!(g)
+	warn("turn ", turn)
 
 	if turn == 1
 		push!(cmds, H.make_ship())
@@ -26,13 +29,21 @@ while true
 
 	#calculate where ships want to move
 	for s in me.ships
-		ms, dir, cost_here2there = mapscore(g.halite, s, me.shipyard, miningthreshold)
-		ms_within_reach = filterscores(ms, cost_here2there, s.halite)
-		yx = pickbestsquare(ms_within_reach)
+		warn("ship ", s.id, " pos=", Tuple(s.p), " halite=", s.halite)
+		if !canmove(s, g.halite)
+			push!(cmds, H.move(s, H.STAY_STILL))
+			warn("can't move ", Salboai.leavecost(g.halite[s.p]), " > ", s.halite)
+		else
+			ms, dir, cost_here2there = mapscore(g.halite, s, me.shipyard, miningthreshold)
+			warn(g.halite[s.p[1] .+ (-1:1), s.p[2] .+ (-1:1)])
+			#ms_within_reach = filterscores(ms, cost_here2there, s.halite)
+			#yx = pickbestsquare(ms_within_reach)
+			yx = pickbestsquare(ms)
 
-		#need to add a manhattan distance cost from ship to all map and from shipyard to all map. sum them
-		#and multiply by something to not just walk away.
-		push!(cmds, H.move(s, dir[yx]))
+			#need to add a manhattan distance cost from ship to all map and from shipyard to all map. sum them
+			#and multiply by something to not just walk away.
+			push!(cmds, H.move(s, dir[yx]))
+		end
 		#push!(cmds, H.move(s, 1))
 	end
 
