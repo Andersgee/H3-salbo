44b3e467cdc410c570878793132bc3bae0232b32
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2018-10-28 08:49:03 +0100

    max_turns is integer

--- salboai/common.jl
+++ salboai/common.jl
@@ -1,6 +1,6 @@
 splitview(A; dims=ndims(A)) = view.((A,), (d == dims ? (1:size(A,d)) : (:) for d in 1:ndims(A))...)
 
-max_turns(g::H.GameMap) = 451 + (size(g.halite, 1) - 48)*25/8
+max_turns(g::H.GameMap) = 451 + div((size(g.halite, 1) - 48)*25, 8)
 
 warn_io = Base.stderr
 function warn(s...)
