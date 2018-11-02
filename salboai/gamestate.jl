0c614553d26388202ffbf88bd4bd94876cc7315c
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2018-11-02 10:33:40 +0100

    gamestate: ships_to_dropoff

--- salboai/gamestate.jl
+++ salboai/gamestate.jl
@@ -1,13 +1,15 @@
 struct GameState
     ship_dropoff_turn::Dict{Int,Int}
+    ships_to_dropoff::Set{Int}
 end
 
-GameState() = GameState(Dict{Int,Int}())
+GameState() = GameState(Dict{Int,Int}(), Set{Int}())
 
 function update_game_state!(S::GameState, h::H.GameMap, turn::Int)
     # clear killed ships
     ship_ids = [ship.id for (id,player) in h.players for ship in player.ships]
-    delete!(S.ship_dropoff_turn, setdiff(collect(keys(S.ship_dropoff_turn)), ship_ids))
+    delete!.((S.ship_dropoff_turn,), setdiff(collect(keys(S.ship_dropoff_turn)), ship_ids))
+    intersect!(S.ships_to_dropoff, ship_ids)
 
     # update dropoff ticks
     for (id,player) in h.players
@@ -16,6 +18,7 @@ function update_game_state!(S::GameState, h::H.GameMap, turn::Int)
         for ship in player.ships
             if ship.p ∈ dropoffs || !haskey(S.ship_dropoff_turn, ship.id)
                 S.ship_dropoff_turn[ship.id] = turn
+                delete!(S.ships_to_dropoff, ship.id)
             end
         end
     end
